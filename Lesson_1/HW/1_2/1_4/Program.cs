4bf631a548b1dda8c8b943945bae1f95a1915349
GDIfox <gidmitriy@me.com>
2023-06-25 10:44:48 +0300

    feat: complite lesson and add task Home Work

---
+++ Lesson_1/HW/1_2/1_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+//     и выдаёт максимальное из этих чисел.
+
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b & c < a)
+{
+    Console.WriteLine("Max = {0}", a);
+}
+else if (a < b & b > c)
+{
+    Console.WriteLine("Max = {0}", b);
+}
+else if (c > a & c > b)
+{
+    Console.WriteLine("Max = {0}", c);
+}
---
+++ Lesson_1/HW/1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+//    какое число большее, а какое меньшее.
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a < b)
+{
+    Console.WriteLine("Max = {0}", b);
+}
+else
+{
+    Console.WriteLine("Max = {0}", a);
+}
+
