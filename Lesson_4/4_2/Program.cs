ecfb30652786a4d3e80453f62189179191b592ea
GDIfox <gidmitriy@me.com>
2023-07-04 09:07:47 +0300

    add: Work on Seminar

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+
+
+Console.WriteLine("Введите число");
+
+int Count(int num)
+{
+    int result = 0;
+    for (int i = 1; num != 0; i++)
+    {
+        num /= 10;
+        result = i;  
+    }
+return result;  
+}
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Count(A));
+
+
+// с произведением
+
+// nt SumNum(int num)
+// {
+//     int all_sum = 1;
+//     for (int i = 1; i <= num; i++)
+//         all_sum = all_sum*i;
+
+//     return all_sum;
+// }
+
+// int A = int.Parse(Console.ReadLine()!);
+// int result = SumNum(A);
+// Console.WriteLine(result);
