fde6fb893ba5b3db50cd8443bbe8ba193285ba3f
GDIfox <gidmitriy@me.com>
2023-07-04 09:06:20 +0300

    add: Work on Seminar

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает чсило А и выдается сумма от 1 до А
+
+int SumNum(int num)
+{
+    int all_sum = 0; // если есть произведение - то начинаем с 1
+    for (int i = 1; i <= num; i++)
+    all_sum += i;
+
+    return all_sum;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = SumNum(A);
+Console.WriteLine(result);
+
+
+
+
+
