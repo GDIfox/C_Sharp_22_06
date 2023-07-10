70b65c6acfbdf63cc00eb065f54dc1260240781f
GDIfox <gidmitriy@me.com>
2023-07-10 12:10:23 +0300

    add: class work

---
+++ Lesson_5/5_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие 
+// отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+void ChangeNums(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;  
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();    
+}
+
+int[] EightMass(int size, int from, int to)
+{
+    int[] arr = new int[size]; //формирование нового массива размером size
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to + 1); // + 1 = это сдвиг справа, т.к. указав "край" чтобы он же в массив и попал
+    return arr;
+}
+
+int num = int.Parse(Console.ReadLine()!);    // В EightMass (size, from, to) - параметры должны отличаться!!! 
+int start = int.Parse(Console.ReadLine()!);  // - от переменных = Это очень важно!!!!
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = EightMass(num, start, stop);
+Print(mass);
+ChangeNums(mass);
+Print(mass);
