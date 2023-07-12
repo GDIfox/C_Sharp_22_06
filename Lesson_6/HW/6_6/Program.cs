cf38c339e4e8d7811219742b312098ee0c7b4f6e
GDIfox <gidmitriy@me.com>
2023-07-12 10:23:42 +0300

    add - if

--- Lesson_6/HW/6_6/Program.cs
+++ Lesson_6/HW/6_6/Program.cs
@@ -13,7 +13,14 @@ Console.Write("Введите число b2: ");
 double b2 = double.Parse(Console.ReadLine()!);
 Console.Write("Введите число k2: ");
 double k2 = double.Parse(Console.ReadLine()!);
-double x = (b2 - b1) / (k1 - k2);
-double y = k1 * x + b1;
 
-Console.Write($"Точка пересечения= ({x};{y})");
+if ((k1 - k2) == 0)
+{
+    Console.WriteLine("Деление на ноль невозможно");
+}
+else
+{
+    Console.Write(
+        $"Точка пересечения= ({(b2 - b1) / (k1 - k2)};{(k1 * ((b2 - b1) / (k1 - k2))) + b1})"
+    );
+}
