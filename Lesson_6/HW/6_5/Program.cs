6da57469416b85cfc33de95b70bb1c26246672f2
GDIfox <gidmitriy@me.com>
2023-07-16 12:12:31 +0300

    add class work- Amn = m+n

--- Lesson_6/HW/6_5/Program.cs
+++ Lesson_6/HW/6_5/Program.cs
@@ -40,3 +40,28 @@ int[] mas = RandomNums(dlmas);
 Print(mas);
 Console.WriteLine();
 PlusNums(mas);
+
+
+//----------variant-------------------------
+
+// Console.Write("Сколько чисел?");
+// int M = int.Parse(Console.ReadLine()!);
+// int res=0;
+
+// for (int i = 1; i <= M; i++)
+// {
+//     Console.WriteLine($"Введите {i} число ");
+//     int a = int.Parse(Console.ReadLine()!);
+
+//     if (a==0)
+//     {
+//                 Console.WriteLine("Введеное число равно 0");
+//                 break;
+//     }
+
+//     else if (a>0) res=res+1;
+// }
+
+
+
+// Console.WriteLine($"Количество положительных чисел = {res} ");
