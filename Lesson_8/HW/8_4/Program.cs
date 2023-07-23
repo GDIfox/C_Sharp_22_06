657c1b77822b247baf9371a593eddfe49aaef839
GDIfox <gidmitriy@me.com>
2023-07-23 15:29:54 +0300

    add: home work - ChangeMaxMin

---
+++ Lesson_8/HW/8_4/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void RandomArray(int[,] array)
+{
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 50);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j], 4}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void ChangeMaxMin(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - j - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    (array[i, k], array[i, k + 1]) = (array[i, k + 1], array[i, k]);
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Введите число строк массива: ");
+int str = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число столбцов массива: ");
+int col = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+int[,] NewArray = new int[str, col];
+
+RandomArray(NewArray);
+PrintArray(NewArray);
+Console.WriteLine();
+ChangeMaxMin(NewArray);
+PrintArray(NewArray);
