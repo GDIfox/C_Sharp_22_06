3f0914f54d5c624be85bd69bf313f81ac97733a3
GDIfox <gidmitriy@me.com>
2023-07-24 17:15:58 +0300

    add: HW-recursion sumnums from M to N

---
+++ Lesson_9/HW/9_6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int PrintNumber(int M, int N)
+{
+    if (M <= N)
+    {
+        return N + PrintNumber(M, N - 1);
+    }
+    // else if (M > N)
+    // {
+    //     return N + PrintNumber(M, N + 1);
+    // }
+
+    return 0;
+}
+Console.WriteLine($"{PrintNumber(4, 8)}");
